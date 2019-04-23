cf1bbb7ffb478f49e4d3076fccfef81cde2a2487
Haseeb Ali <haseebali1@gmail.com>
2019-04-22 20:17:50 -0700

    added more packages to install

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ wallpaper=~/Pictures
 
 install_program()
 {
-    sudo pacman -S i3-wm i3status dmenu vlc firefox tor libreoffice-fresh clamav feh compton flashplugin vim pulseaudio pulseaudio-alsa xorg xorg-xinit rxvt-unicode dunst libnotify noto-fonts-emoji numlockx networkmanager network-manager-applet i3lock curl
+    sudo pacman -S i3-wm i3status dmenu vlc firefox tor libreoffice-fresh clamav feh compton flashplugin vim pulseaudio pulseaudio-alsa xorg xorg-xinit rxvt-unicode dunst libnotify noto-fonts-emoji numlockx networkmanager network-manager-applet i3lock curl cronie deja-dup graphicsmagick mariadb php apache php-apache phpmyadmin ranger transmission-gtk ufw virtualbox virtualbox-guest-utils 
 }
 
 create_directories()
